910c83bde2788489227afadfa3c1f7ad1e4acde0
BlackTea <1571106964@qq.com>
2026-07-03 22:26:57 +0800

    solve(arrays): MaximumWealth_1672

---
+++ arrays/MaximumWealth_1672.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class MaximumWealth_1672 {
+    static class Solution {
+        public int maximumWealth(int[][] accounts) {
+            int m = accounts.length;
+            int max = 0;
+            for (int i = 0; i < m; i++) {
+                int sum = 0;
+                int n = accounts[i].length;
+                for (int j = 0; j < n; j++) {
+                    sum += accounts[i][j];
+                }
+                if (sum > max) {
+                    max = sum;
+                }
+            }
+            return max;
+        }
+    }
+}
