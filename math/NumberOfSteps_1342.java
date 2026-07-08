106752577b7223c24c4808bf6a26f6919f04c924
BlackTea <1571106964@qq.com>
2026-07-08 22:33:22 +0800

    solve(math): NumberOfSteps_1342

---
+++ math/NumberOfSteps_1342.java
@@ -0,0 +1,18 @@
+package math;
+
+public class NumberOfSteps_1342 {
+    static class Solution {
+        public int numberOfSteps(int num) {
+            int count = 0;
+            while (num != 0) {
+                if (num % 2 == 0) {
+                    num = num / 2;
+                } else {
+                    num = num - 1;
+                }
+                count++;
+            }
+            return count;
+        }
+    }
+}
