7dd0db70c0681cdff9142e8f4bbbd92a23a88d4f
BlackTea <1571106964@qq.com>
2026-08-06 23:23:36 +0800

    solve(trees): SumOfLeftLeaves_0404

---
+++ trees/SumOfLeftLeaves_0404.java
@@ -0,0 +1,38 @@
+package trees;
+
+public class SumOfLeftLeaves_0404 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution {
+        public int sumOfLeftLeaves(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int sum = 0;
+
+            if (root.left != null && root.left.left == null && root.left.right == null) {
+                sum += root.left.val;
+            }
+
+            return sum + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        }
+    }
+}
