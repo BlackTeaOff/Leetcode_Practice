b2568e476bdd4be23da1db6d1ece33c99cf8e3d4
BlackTea <1571106964@qq.com>
2026-08-18 22:11:23 +0800

    refactor(trees): MergeTrees_0617

--- trees/MergeTrees_0617.java
+++ trees/MergeTrees_0617.java
@@ -20,6 +20,25 @@ public class MergeTrees_0617 {
         }
     }
 
+    static class MySolution {
+        public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+            // 前两个 if 可以删掉, 因为下面两个 if 已经涵盖了所有情况
+            if (root1 == null && root2 == null) {
+                return null;
+            }
+            if (root1 != null && root2 != null) {
+                root1.val += root2.val;
+            } else if (root1 == null) {
+                return root2;
+            } else if (root2 == null) {
+                return root1;
+            }
+            root1.left = mergeTrees(root1.left, root2.left);
+            root1.right = mergeTrees(root1.right, root2.right);
+            return root1;
+        }
+    }
+
     static class Solution {
         public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
             if (root1 == null) {
