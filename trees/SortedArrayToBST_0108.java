442afbe3a63eae08133fb21559d711822a70f2dd
BlackTea <1571106964@qq.com>
2026-08-18 20:43:38 +0800

    refactor(trees): SortedArrayToBST_0108

--- trees/SortedArrayToBST_0108.java
+++ trees/SortedArrayToBST_0108.java
@@ -20,6 +20,31 @@ public class SortedArrayToBST_0108 {
         }
     }
 
+    public static class MySolution {
+        TreeNode recursion(int[] nums, int left, int right) {
+            if (left > right) {
+                return null;
+            }
+            // 避免溢出, 计算中点用 left + (right - left) / 2
+            // 1/2 left + 1/2 right
+            int pos = (left + right) / 2;
+            TreeNode middle = new TreeNode(nums[pos]);
+            middle.left = recursion(nums, left, pos - 1);
+            middle.right = recursion(nums, pos + 1, right);
+            return middle;
+        }
+
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return recursion(nums, 0, nums.length - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { -10, -3, 0, 5, 9 };
+        MySolution mySolution = new MySolution();
+        mySolution.sortedArrayToBST(nums);
+    }
+
     static class Solution {
         public TreeNode sortedArrayToBST(int[] nums) {
             return helper(nums, 0, nums.length - 1);
