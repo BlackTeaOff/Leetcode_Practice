9dba73088e8d06fd7d0b99176f8fc33b9da1638a
BlackTea <1571106964@qq.com>
2026-08-24 21:57:31 +0800

    refactor(trees): LowestCommonAncestor_0235

--- trees/LowestCommonAncestor_0235.java
+++ trees/LowestCommonAncestor_0235.java
@@ -20,6 +20,21 @@ public class LowestCommonAncestor_0235 {
         }
     }
 
+    static class MySolution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val > p.val && root.val > q.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else if (root.val < p.val && root.val < q.val) { // else if 可直接换成 if
+                return lowestCommonAncestor(root.right, p, q);
+            } else { // 这个 else 可删掉
+                return root;
+            }
+        }
+    }
+
     static class Solution {
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
             if (root.val > p.val && root.val > q.val) {
