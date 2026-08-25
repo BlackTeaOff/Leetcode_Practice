dbb501ac9e7e9fae9aaaf4353ee729ba3dd6e972
BlackTea <1571106964@qq.com>
2026-08-25 22:13:06 +0800

    refactor(trees): TwoSumBST_0653

--- trees/TwoSumBST_0653.java
+++ trees/TwoSumBST_0653.java
@@ -17,6 +17,21 @@ public class TwoSumBST_0653 {
         }
     }
 
+    static class MySolution {
+        Set<Integer> hashSet = new HashSet<>();
+
+        public boolean findTarget(TreeNode root, int k) {
+            if (root == null) {
+                return false;
+            }
+            if (hashSet.contains(root.val)) {
+                return true;
+            }
+            hashSet.add(k - root.val);
+            return findTarget(root.left, k) || findTarget(root.right, k);
+        }
+    }
+    
     static class Solution {
         private Set<Integer> set = new HashSet<>();
 
