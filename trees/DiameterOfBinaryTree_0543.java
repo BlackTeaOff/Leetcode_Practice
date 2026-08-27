270e671274751787151d326507b604079f9217b4
BlackTea <1571106964@qq.com>
2026-08-27 20:25:32 +0800

    refactor(trees): DiameterOfBinaryTree_0543

--- trees/DiameterOfBinaryTree_0543.java
+++ trees/DiameterOfBinaryTree_0543.java
@@ -20,6 +20,25 @@ public class DiameterOfBinaryTree_0543 {
         }
     }
 
+    static class MySolution {
+        int diameter = 0;
+
+        int recursion(TreeNode node) {
+            if (node == null) {
+                return 0;
+            }
+            int leftDepth = recursion(node.left);
+            int rightDepth = recursion(node.right);
+            diameter = Math.max(diameter, leftDepth + rightDepth);
+            return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+        }
+
+        public int diameterOfBinaryTree(TreeNode root) {
+            recursion(root);
+            return diameter;
+        }
+    }
+
     static class Solution {
         private int maxDiameter = 0;
 
